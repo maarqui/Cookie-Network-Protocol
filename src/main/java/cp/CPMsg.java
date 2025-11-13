9dbb283c77f47321ee2d78d639d2a3ac1e343e72
maarqui <danielmarquinoperez@gmail.com>
2025-11-13 18:39:46 +0100

    Improvements on the parse method

--- src/main/java/cp/CPMsg.java
+++ src/main/java/cp/CPMsg.java
@@ -18,16 +18,19 @@ class CPMsg extends Msg {
     @Override
     protected Msg parse(String sentence) throws IWProtocolException {
         CPMsg parsedMsg;
-        if(!sentence.startsWith(CP_HEADER))
-            throw new IllegalMsgException();
+        if(!sentence.startsWith(CP_HEADER)) throw new IllegalMsgException();
 
         String[] parts = sentence.split("\\s+", 2);
-        if(parts.length < 2)
-            throw new IllegalMsgException();
+        if(parts.length < 2) throw new IllegalMsgException();
+
         if(parts[1].startsWith(CPCookieRequestMsg.CP_CREQ_HEADER)) {
             parsedMsg = new CPCookieRequestMsg();
         } else if(parts[1].startsWith(CPCookieResponseMsg.CP_CRES_HEADER)) {
             parsedMsg = new CPCookieResponseMsg();
+        } else if(parts[1].startsWith(CPCommandMsg.CP_CMD_HEADER)) {
+            parsedMsg = new CPCommandMsg();
+        } else if(parts[1].startsWith(CPCommandResponseMsg.CP_CMD_RES_HEADER)) {
+            parsedMsg = new CPCommandResponseMsg();
         } else {
             throw new IllegalMsgException();
         }
